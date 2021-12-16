e5be69134c4c2ea806d603d06743302cbaf078d3
Arpit <arpit_jain@live.com>
2021-12-16 17:14:26 +0530

    Node basics - event loop

---
+++ call-stack.js
@@ -0,0 +1,84 @@
+// const x = 10;
+// const y = 20;
+
+// const total = x + y;
+
+// console.log(`${x} + ${y} = ${total}`);
+setTimeout(() => {
+
+    function multiply() {
+      console.log("Multiply function");
+      const value = divide();
+      try {
+        throw new Error("custom error in multiply");
+      } catch (error) {
+        console.log(error);
+      }
+      return value * 10;
+    }
+    
+    function divide() {
+      console.log("Divide function");
+      try {
+        new Promise(resolve => resolve('Promise resolved')).then(data => console.log(data));
+        throw new Error("Custom error message");
+      } catch (error) {
+        console.log(error);
+        return 100 / 10;
+      }
+    }
+    
+    function substract() {
+      console.log("substract function");
+      const value = multiply();
+      try {
+        throw new Error("custom error in substract");
+      } catch (error) {
+        console.log(error);
+      }
+      return value - 10;
+    }
+    
+    function addition() {
+      console.log("Addition function");
+      setTimeout(() => console.log("Addition timeout"), 0);
+      const value = substract();
+      try {
+        throw new Error("custom error in addition");
+      } catch (error) {
+        console.log(error);
+      }
+      return value + 10;
+    }
+    
+    function printOut() {
+      let count = 0;
+      const intervalId = setInterval(() => {
+        count++;
+        console.log("Set interval");
+        if(count >= 10) {
+            clearInterval(intervalId);
+        }
+      }, 0);
+      const value = addition();
+      try {
+        throw new Error("custom error in printout");
+      } catch (error) {
+        console.log(error);
+      }
+      console.log("final value", value);
+    }
+    try {
+      printOut();
+      throw new Error("final error");
+    } catch (error) {
+      console.log(error);
+    }
+}, 0);
+// let count = 0;
+// console.time()
+// while(count <= 10000000000) {
+//     count++;
+// }
+// console.timeEnd();
+console.log("FINISH");
--- src/read-stream.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-// fs.readFile("bigfile.txt", {encoding: 'utf8'}, (err, data) => console.log(data))
-const readStream = fs.createReadStream("bigfile.txt", {encoding: 'utf8'});
-
-readStream.pipe(process.stdout);
-
-console.log('reading is complete');
