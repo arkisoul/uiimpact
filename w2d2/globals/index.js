d8bca6a4679a68e540a598623282fe714ea1c655
Arpit <arpit_jain@live.com>
2021-12-21 21:57:01 +0530

    Globals: process and buffer

--- w2d2/globals/index.js
+++ w2d2/globals/index.js
@@ -1,8 +1,24 @@
 const process = require("process");
-
-console.log(process.env)
+// console.log(process.env)
 // require('dotenv').config();
 const dotenv = require('dotenv');
 const path = require("path");
+// console.log(__dirname, path.join(__dirname, "..", ".env"));
 dotenv.config({ path: path.join(__dirname, "..", ".env") });
-console.log(process.env.DB_HOST, process.env.DB_USERNAME);
+// const fs = require('fs');
+// const data = fs.readFileSync(path.join(__dirname, "..", ".env"));
+// console.log(data.toString().split("\n"))
+// console.log(process.env.DB_HOST, process.env.DB_USERNAME);
+// console.log(process.version, process.versions, process.release, process.platform)
+// console.log(process.memoryUsage())
+
+// const Buffer = require("buffer");
+// Uint8Array
+// Uint32Array
+// Buffer
+const bf1 = Buffer.alloc(10)
+console.log(bf1)
+const bf2 = Buffer.alloc(10, 1);
+console.log(bf2);
+const hw = Buffer.from("Hello world", 'utf8');
+console.log(hw)
