f2b59b3b5c7c968cb123e3ccb6ef4833bc3dd7b6
Arpit <arpit_jain@live.com>
2021-12-22 17:29:03 +0530

    Handling routes with node http server

--- w2d3/server/index.js
+++ w2d3/server/index.js
@@ -4,9 +4,20 @@ const server = http.createServer(); // create a server
 
 server.on("request", (request, response) => {
     // console.log(request, response);
-    request.on('data', (chunk) => console.log('req data', chunk))
-    response.write("Hello world!");
-    response.end()
+    const method = request.method;
+    const url = request.url;
+    console.log(method, url)
+    request.on('data', (chunk) => console.log('req data', chunk.toString()))
+    if(method === 'GET' && url === '/') {
+        response.write("You are visiting the root url");
+        response.end()
+    } else if(method === 'POST' && url === '/') {
+        response.write("You are visiting the root url with POST method");
+        response.end();
+    } else {
+        response.write("End of server. No matching rout found");
+        response.end();
+    }
 })
 
 server.listen(3000, () => console.log(`HTTP server started listening at http://localhost:3000`)); // start listening on a port
