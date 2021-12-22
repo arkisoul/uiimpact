af57d6164544e615d9750a1717cf4e510a041b5e
Arpit <arpit_jain@live.com>
2021-12-22 16:28:38 +0530

    Transform stream ex using zlib

--- w2d3/streams/transform-duplex.js
+++ w2d3/streams/transform-duplex.js
@@ -1,9 +1,11 @@
 const { createWriteStream, createReadStream } = require("fs");
 const zlib = require("zlib");
 
-createReadStream("README.md")
-  .on("data", (chunk) => console.log(chunk.toString()))
-  .on("end", () => console.log("reading end"))
+// .zip, .rar, .tar, .tar.gz, .7zip
+
+createReadStream("./w2d3/test.txt", {start: 10, end: 40})
+//   .on("data", (chunk) => console.log(chunk.toString()))
+//   .on("end", () => console.log("reading end"))
   .pipe(zlib.createGzip())
-  .on("end", () => console.log("compression completed"))
-  .pipe(createWriteStream("README.md.gz"));
+//   .on("end", () => console.log("compression completed"))
+  .pipe(createWriteStream("./w2d3/test.txt.gz"));
